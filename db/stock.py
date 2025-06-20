8d7add921c97d745c6d026f7daeb752fd2b7431b
Vyquos <75266055+Vyquos@users.noreply.github.com>
2025-06-20 03:02:12 +0200

    Include validity timestamp in db stock price getters

--- db/stock.py
+++ db/stock.py
@@ -6,7 +6,7 @@ def get_price_current(db: Connection, stock_id: int):
     with db.cursor(cursor_type=READ_ONLY) as cursor:
         # FIXME fix time check by converting to unix timestamp
         cursor.execute(
-            """SELECT price FROM prices
+            """SELECT price, valid_after FROM prices
             WHERE
                 stock_id = (?) AND valid_after <= NOW()
             ORDER BY valid_after DESC
@@ -16,11 +16,11 @@ def get_price_current(db: Connection, stock_id: int):
         return cursor.fetchone()
 
 
-def get_price_history(db: Connection, stock_id: int, num_entries=1):
+def get_price_history(db: Connection, stock_id: int, num_entries: int = 1):
     with db.cursor(cursor_type=READ_ONLY) as cursor:
         # FIXME fix time check by converting to unix timestamp
         cursor.execute(
-            """SELECT price FROM prices
+            """SELECT price, valid_after FROM prices
             WHERE
                 stock_id = (?) AND valid_after <= NOW()
             ORDER BY valid_after DESC
@@ -30,15 +30,19 @@ def get_price_history(db: Connection, stock_id: int, num_entries=1):
         return cursor.fetchmany(num_entries)
 
 
-def get_price_preview(db: Connection, stock_id: int, num_entries=1):
+def get_price_preview(db: Connection, stock_id: int, num_entries: int = 1):
     with db.cursor(cursor_type=READ_ONLY) as cursor:
         # FIXME fix time check by converting to unix timestamp
         cursor.execute(
-            """SELECT price FROM prices
+            """SELECT price, valid_after FROM prices
             WHERE
                 stock_id = (?) AND valid_after > NOW()
             ORDER BY valid_after ASC
             """,
             (stock_id,)
         )
+        # FIXME returning more than one entry is contrary to the HTTP API which
+        # only allows retrieving / editing of one preview value. This causes
+        # inconsistency in behavior because cursor.fetchmany() handles no
+        # results differently from cursor.fetchone().
         return cursor.fetchmany(num_entries)
