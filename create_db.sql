96640044478741173645d17e36ee5724cc9a0fd3
Vyquos <75266055+Vyquos@users.noreply.github.com>
2025-07-03 19:09:22 +0200

    refactor(db): Remove useless timezone setting in db creation script

--- create_db.sql
+++ create_db.sql
@@ -5,6 +5,3 @@ CREATE DATABASE IF NOT EXISTS dau_jones;
 GRANT ALL PRIVILEGES ON dau_jones.* TO dau_jones_admin;
 GRANT dau_jones_admin TO dau_jones;
 SET DEFAULT ROLE dau_jones_admin FOR dau_jones;
-
-USE dau_jones;
-SET @@time_zone = '+00:00';
